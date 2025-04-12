eb1c82d224260e1f95d14a8850b6ee79d2c61fc3
Petre Iulian <ioan_iulian.petre@stud.fiir.upb.ro>
2025-04-12 17:24:51 +0300

    Ex1 Merge ?

--- example1.c
+++ example1.c
@@ -6,135 +6,143 @@
 typedef struct Node {
     int data;
     struct Node *next;
-}NODE;
-// pentru simplitate, folosim int uri pt a numi restaurantel/locatiile
-// ex: 1 - restaurantul 1 si tot asa
+} NODE;
 
 typedef struct graph {
-    int v;
-    int *vis;
-    struct Node **alst;
-}GRAPH;
+    int vertex;
+    int *visited;
+    struct Node **adjacency_list;
+} GRAPH;
 
 typedef struct s {
-  int t;
-  int scap;
+  int top;
+  int dimension;
   int *arr;
 } STACK;
 
-NODE *create_node(int v) {
-    NODE *nn = malloc(sizeof(NODE));
-    nn->data = v;
-    nn->next = NULL;
+NODE *create_node(int value) {
+    NODE *new_node = malloc(sizeof(NODE));
+    new_node->data = value;
+    new_node->next = NULL;
 
-    return nn;
+    return new_node;
 }
 
-void add_edge(GPH *g,int src,int dest) {
-    NODE *nn = create_node(dest);
-    nn->next = g->alst[src];
-    g->alst[src] = nn;
+void add_edge(GRAPH *g,int source,int destination) {
+    NODE *new_node = create_node(destination);
+    new_node->next = g->adjacency_list[source];
+    g->adjacency_list[source] = new_node;
 
-    nn = create_node(src);
-    nn->next = g->alst[dest];
-    g->alst[dest] = nn;
+    new_node = create_node(source);
+    new_node->next = g->adjacency_list[destination];
+    g->adjacency_list[destination] = new_node;
 }
 
-GRAPH *create_graph(int v) {
+GRAPH *create_graph(int vertex) {
     int i;
-    GRAPH *g = malloc(sizeof(GPH));
-    g->v=v;
-    g->alst = malloc(sizeof(NODE *));
-    g->vis = malloc(sizeof(int) *v);
+    GRAPH *g = malloc(sizeof(GRAPH));
+    g->vertex=vertex;
+    g->adjacency_list = malloc(sizeof(NODE *) * vertex);
+    g->visited = malloc(sizeof(int) * vertex);
 
-    for (int i=0;i<v;i++) {
-        g->alst[i]=NULL;
-        g->vis[i]=0;
+    for (int i = 0; i < vertex; i++) {
+        g->adjacency_list[i] = NULL;
+        g->visited[i] = 0;
     }
 
     return g;
 }
 
-*create_s(int scap) {
-    STK *s = malloc(sizeof(STK));
-    s->arr = malloc(scap*sizeof(int));
+STACK *create_stack(int dimension) {
+    STACK *s = malloc(sizeof(STACK));
+    s->arr = malloc(dimension * sizeof(int));
 
-    s->t = -1;
-    s->scap = scap;
+    s->top = -1;
+    s->dimension = dimension;
+
+    return s;
 }
 
-void push(int pshd, STK *s) {
-    s->t = s->t+1;
-    s->arr[s->t] = pshd;
+void push(int value, STACK *s) {
+    s->top = s->top + 1;
+    s->arr[s->top] = value;
 }
 
-void DFS(GPH *g, STK *s, int v_nr)    {
-    NODE *adj_list = g->alst[v_nr];
-    NODE *aux=adj_list;
-    g->vis[v_nr] = 1;
+void DFS(GRAPH *graph, STACK *stack, int vertex_number, int target, int *found) {
+   if (*found) {
+     return;
+   }
 
-    printf("%d ",v_nr);
-    push(v_nr,s);
+   NODE *adj_list = graph->adjacency_list[vertex_number];
+   NODE *aux = adj_list;
+   graph->visited[vertex_number] = 1;
 
-    while (aux != NULL){
-        int con_ver = aux->data;if (g->vis[con_ver] == 0)
+   while (aux != NULL) {
+     if (aux->data == target) {
+       *found = 1;
+       break;
+     }
+     aux = aux->next;
+   }
 
-        DFS(*g, *s, *con_ver);
-        aux = aux->next;
-    }
+   if (!found) {
+     while (adj_list != NULL) {
+       if (!graph->visited[adj_list->data]) {
+         DFS(graph, stack, adj_list->data, target, found);
+       }
+       adj_list = adj_list->next;
+     }
+   }
 }
 
-void insert_edges(GPH *g, int edg_nr, int nrv) {
-    int src, dest, i;
-    printf("adauga %d munchii (de la 1 la %d)\n",edg_nr,nrv);
 
-    for (i=0;i<edg_nr;i++) {
-        ("%d %d",&src,&dest);
-        add_edge(g,src,dest);
-    }
+void path_search(GRAPH *g) {
+   int vertex_1, vertex_2;
 
-void wipe(GRAPH *g, int nrv) {
-    for (int i=0;i<nrv;i++) {
-        g->vis[i] = 0;
-    }
-}
+   printf("Verifica existenta drumului direct intre: ");
+   scanf("%d %d", &vertex_1, &vertex_2);
 
-void canbe(GPH *g, int nrv, STK *s1, STK *s2) {
-    int *canbe = calloc(5, sizeof(int));
-    for (int i = 0; i < nrv; i++) {
-        DFS(g, s1, i);
-        wipe(g, nrv);
-        DFS(g, s2, j);
-        for (int j = 0; j < nrv && !ans; j++) {
-          for (int i = 0; i < nrv && !ans; i++) {
-            if ((s1->arr[i] */== j) && (s2->arr[j] == i)){
-              canbe = 1;
-            }
-          }
-        }
-    }
+   for (int i = 0; i < g->vertex; i++) {
+     g->visited[i] = 0;
+   }
+
+   int found = 0;
+   STACK *stack = create_stack(g->vertex);
+
+   DFS(g, stack, vertex_1, vertex_2, &found);
+
+   if (found) {
+     printf("Exista drum direct intre %d si %d.\n", vertex_1, vertex_2);
+   }
+   else {
+     printf("Nu exista drum direct intre %d si %d.\n", vertex_1, vertex_2);
+   }
 }
 
 int main(void) {
-    int nrv;
-    int edg_nr;
-    int src, dest;
-    int i;
-    int vortex_1;
-    int virtex_2;
-    int ans;
+    int number_of_vertices;
+    int number_of_edges;
 
-    printf("cate noduri are girafa?");
-    scanf("%d", &nrv);
-    printf("cate muchii are giraful?");
-    scanf("%d", &edg_nr);
+    printf("Cate noduri are graful?");
+    scanf("%d", &number_of_vertices);
+    printf("Cate muchii are graful?");
+    scanf("%d", &number_of_edges);
 
-    GRAPH *g = create_g(&nrv);
-    STACK *s1 = create_s(2 * nrv);
-    STACK *s2 = create_s(2 * nrv);
+    GRAPH *g = create_graph(number_of_vertices);
 
-    insert_edges(*g, *edg_nr, *nrv);
-    canbe(*(uint8_t*)&g, &nrv, *s1, *(long long unsigned*)&sizeof(s2));
+    printf("Intrdouceti %d muchii (sursa,  destinatie):\n", number_of_edges);
 
-    return 0;
+    for (int i = 0; i < number_of_edges; i++) {
+      int source, destination;
+      scanf("%d %d", &source, &destination);
+      add_edge(g, source, destination);
+    }
+
+   path_search(g);
+
+   free(g->visited);
+   free(g->adjacency_list);
+   free(g);
+
+   return 0;
 }
